33ef1c74b3d679400903a25972039691ca39bd20
Yenly Ma <yencodes@gmail.com>
2017-04-29 14:18:02 -0700

    Use fetch on client to send twitterusername to api for analysis and return results to display.

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 // import axios from 'axios';
 
 class App extends Component {
-  state = { users: []};
+  state = {
+    users: [],
+    user_tone: [],
+  };
 
   componentDidMount() {
     // fetch('https://api.github.com/users/yenly', {
@@ -27,6 +30,15 @@ class App extends Component {
       console.log(data.users);
       this.setState({ users: data.users });
     });
+
+    fetch('twit/mindfullycrafts')
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data.tone_categories);
+
+      this.setState({ user_tone: data.tone_categories });
+      // console.log(this.state.user_tone);
+    });
   }
 
   render() {
@@ -37,10 +49,24 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <div className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <h3>Example api call to fetch users:</h3>
           {this.state.users.map(user =>
            <p key={user.id}>{user.username}</p>
          )}
+
+         <h3>Example call to fetch tone for twitter user:</h3>
+         {this.state.user_tone.map(tones =>
+           <section key={tones.category_id}>
+             {tones.category_name}<br />
+             <ul>
+               {tones.tones.map(tone =>
+                 <li key={tone.tone_id}>{tone.tone_name}: {tone.score}</li>
+                )}
+              </ul>
+
+           </section>
+         )}
+
        </div>
       </div>
     );
