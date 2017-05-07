1b729a03b069de5696a50018720512e1b8d775e0
Yenly Ma <yencodes@gmail.com>
2017-05-07 10:00:43 -0700

    Configure victory chart to use my labels for axis.

--- client/src/components/tone_chart.js
+++ client/src/components/tone_chart.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
 
 const ToneChart = ({userTone}) => {
+  const toneLabels = userTone.tones.map(tone => {
+    return tone.tone_name;
+  });
+  console.log(toneLabels);
+
   return (
     <section>
       <VictoryChart
         domainPadding={20}>
-        {/* <VictoryAxis
+        <VictoryAxis
           dependentAxis
+          style={{
+            tickLabels: {fontSize: 12, padding: 5}
+          }}
+          tickValues={toneLabels} />
+        <VictoryAxis
           tickValues={[0, 0.2, 0.4, 0.6, 0.8, 1]}
           tickFormat={[0, 0.2, 0.4, 0.6, 0.8, 1]}
-        /> */}
+        />
         <VictoryBar
           data={userTone.tones}
-          // horizontal={true}
+          horizontal={true}
           x="tone_name"
           y="score" />
       </VictoryChart>
